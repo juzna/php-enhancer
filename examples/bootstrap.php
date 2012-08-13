d9e8d616ba0d49d9df9b86f5b63ed26ccc6a6232
Filip Procházka <hosiplan@gmail.com>
2012-08-13 17:51:16 +0200

    debugger

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
-// Register enhancer stream wrapper
-use Enhancer\EnhancerStream;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 $loader->add('Enhancer', __DIR__ . '/../src/');
 
 // allow dump()
+Nette\Diagnostics\Debugger::$strictMode = TRUE;
 Nette\Diagnostics\Debugger::$productionMode = FALSE;
+Nette\Diagnostics\Debugger::$maxLen = 10000;
 
 // Add new wrapper
 if ( ! stream_wrapper_register('enhance', 'Enhancer\\EnhancerStream')) {
