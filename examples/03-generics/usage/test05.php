984868b43daf4ad8e9763b583e68f3b1f7c896db
Jan Dolecek <juzna.cz@gmail.com>
2012-08-14 22:59:36 +0200

    fixed test

---
+++ examples/03-generics/usage/test05.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class Collection<E> implements IteratorAggregate
+{
+	private $items;
+
+	public function add(E $item)
+	{
+		$this->items[] = $item;
+	}
+
+	public function getIterator()
+	{
+		return new ArrayIterator($this->items);
+	}
+}
+
+
+class Factory<E>
+{
+	public function create($id) {
+		return new E($id);
+	}
+
+	public function createCollection(array $ids) {
+		$c = new Collection<E>();
+		foreach ($ids as $id) {
+			$c->add(new E($id));
+		}
+		return $c;
+	}
+
+}
+
+$f = new Factory<ORM\Entity\User>($em);
+
