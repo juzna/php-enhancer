fc5ffce94f2cd2a0d1ed53fde40e0ba2a05a30ac
Jan Dolecek <juzna.cz@gmail.com>
2012-08-21 10:58:26 +0200

    notes

--- examples/04-class-enhancer/classes/Movie.php
+++ examples/04-class-enhancer/classes/Movie.php
@@ -4,6 +4,8 @@ use ORM\Entity;
 
 class Movie extends ORM\Entity
 {
+	// This will call static method attr(...), which can provide new properties, methods, etc.
+	//  You can call any static method here
 	attr('name', 'string');
 	attr('rating', 'float');
 
