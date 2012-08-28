934a8288bbd0072f9560f82c4d45490c8ce51a60
unu <unumin@gmail.com>
2012-08-28 11:06:15 +0200

    updated LadyPHP examples

--- app/model/Fruit.php
+++ app/model/Fruit.php
@@ -1,23 +1,25 @@
 <?
-# This should not be a .php file, but it is for the sake of autoloader
 
 class Fruit
   var apples = 0
   var numbers = [
     1: 'one',
     2: 'two',
-    3: 'three',
+    3: 'three'
   ]
 
   fn addApples(n = 1)
-    this.apples += n
+    if (n >= 0)
+      this.apples += n
     return this
 
   fn countApples()
     apples = this.apples
     out = 'You have '
-    out .= isset(this.numbers[apples]) ? this.numbers[apples] : apples
-    if (this.apples == 1)
-      return out . ' apple.'
-    else
-      return "$out apples."
+    out .= isset(this.numbers[apples])
+           ? this.numbers[apples] : apples
+    switch (apples)
+      case 1
+        return out . ' apple.'
+      default
+        return "$out apples."
--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -1,9 +1,5 @@
 <?
 # This should not be a .php file, but it is for the sake of autoloader
 
-abstract class BasePresenter extends Nette\Application\UI\Presenter
-  # ladyphp bug - there must be a method with body, otherwise syntax error
-  fn __construct()
-    x = 1
-
-
+# there must be curly brackets, if class is empty
+abstract class BasePresenter extends Nette\Application\UI\Presenter{}
